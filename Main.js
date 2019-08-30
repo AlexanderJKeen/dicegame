caa49a74d3aea1534df7b9b5a3430d52a9aba1d4
CN138 <cn138@CN0138.local>
2019-08-30 15:54:21 +0100

    update

--- Main.js
+++ Main.js
@@ -1,48 +1,62 @@
 const text = document.getElementById('text');
-const image = document.getElementsByTagName('img')
+const image = document.getElementById('roll1')
+const image1 = document.getElementById('roll2')
 
 let total1 = 0
 let total2 = 0
-let images = [`dice1.png`, `dice2.png`, `dice3.png`, `dice4.png`, `dice5.png`, `dice6.png`]
+let images = [`lost.jpeg`, `dice2.png`, `dice3.png`, `dice4.png`, `dice5.png`, `dice6.png`]
 const play1roll = document.getElementById('play1')
 const play2roll = document.getElementById('play2')
 const score1 = document.getElementById('score-1')
 const score2 = document.getElementById('score-2')
+const sound = new Audio()
 
 const rollDice1 = () => {
     let random = Math.floor(Math.random() * 6 + 1)
-    images.src = `./img/${images[random - 1]}`
+    image.src = `./img/${images[random - 1]}`
     score1.innerHTML = total1 += random
     if (random == 1) {
         text.innerHTML = "YOU LOSER!"
         total1 = 0;
+        sound.src = `./loser.wav`
+        sound.play()
+        
     }
     if (total1 >= 21) {
         text.innerHTML = "Player 1  WINS!!!"
         score1.innerHTML = 0;
         total1 = 0;
+        image.src = `./img/dice1.png`
     }
 
 }
-// const rollDice2 = () => {
-//     let random = Math.floor(Math.random() * 6 + 1)
-//     image.src = `./img/${images[random - 1]}`
-//     score2.innerHTML = total2 += random
-//     if (random == 1) {
-//         text.innerHTML = "YOU LOSER!"
-//         score2.innerHTML = 0;
-//         total2 = 0;
-//     }
-//     if (total2 >= 21) {
-//         text.innerHTML = "Player 2 WINS!!!"
-//         total2 = 0;
-//     }
+const rollDice2 = () => {
+    let random = Math.floor(Math.random() * 6 + 1)
+    image1.src = `./img/${images[random - 1]}`
+    score2.innerHTML = total2 += random
+    if (random == 1) {
+        text.innerHTML = "YOU LOSER!"
+        score2.innerHTML = 0;
+        total2 = 0;
+        sound.src = `./loser.wav`
+        sound.play()
+    }
+    if (total2 >= 21) {
+        text.innerHTML = "Player 2 WINS!!!"
+        total2 = 0;
+    }
 
-// }
+}
 
  play1roll.addEventListener('click', () => {
-    rollDice1()
+     rollDice1()
 })
-// play2roll.addEventListener('click', () => {
-//     rollDice2()
-// })
+play2roll.addEventListener('click', () => {
+    rollDice2()
+})
+
+document.addEventListener(keypress, event => {
+    sound.stop()
+
+
+})
